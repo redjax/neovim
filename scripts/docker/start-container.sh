72532eeb7acf8ff69836d77f15730dd10154c403
redjax <none@none.com>
2024-12-17 20:19:09 -0500

    Create Dockerfile
    Add build-container.sh, start-container.sh scripts

---
+++ scripts/docker/start-container.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if ! command -v docker > /dev/null 2>&1; then
+    echo "[WARNING] Docker is not installed."
+    exit 1
+fi
+
+echo ""
+echo "Starting Neovim Docker container"
+echo ""
+
+docker run -it neovim-setup
