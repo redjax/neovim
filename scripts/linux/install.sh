0e43af2ddea76678812df02699787952e4891763
redjax <no@none.com>
2024-12-22 20:13:10 -0500

    Add function to build nvim from source.
    Add INSTALL_NEOVIM_APPIMG env var check to override default source build & use appimg instead. FUSE dependency will only be installed if INSTALL_NEOVIM_APPIMG=1

--- scripts/linux/install.sh
+++ scripts/linux/install.sh
@@ -7,6 +7,9 @@ CWD=$(pwd)
 # Determine the OS type
 OS_TYPE=$(uname -s)
 
+## If INSTALL_NEOVIM_APPIMG=1, do appimg install instead of source build
+INSTALL_NVIM_APPIMG=${INSTALL_NEOVIM_APPIMG:-0}
+
 function eval_last() {
     ## Evaluate the last exit code
     if [[ $1 -eq 0 ]]; then
@@ -113,16 +116,32 @@ NVIM_CONFIG_SRC="${CWD}/config/nvim"
 # echo "[DEBUG] Neovim config source: ${NVIM_CONFIG_SRC}"
 
 ## Path to neovim configuration
-NVIM_CONFIG_DIR="$HOME/.config/nvim"
+NVIM_CONFIG_DIR="${DOTCONFIG_DIR}/nvim"
 # echo "[DEBUG] Neovim config path: ${NVIM_CONFIG_DIR}"
 
 ## Neovim dependency packages installable with dnf
-declare -a NVIM_DNF_DEPENDENCIES=("ripgrep" "xclip" "git" "fzf" "openssl-dev" "fuse")
+declare -a NVIM_DNF_DEPENDENCIES=("ripgrep" "xclip" "git" "fzf" "openssl-dev")
 declare -a NVIM_DNF_GROUP_DEPENDENCIES=("Development Tools" "Development Libraries")
 # echo "[DEBUG] Neovim dependencies installable with apt: ${NVIM_DNF_DEPENDENCIES[@]}"
 
 ## Neovim dependency packages installable with apt
-declare -a NVIM_APT_DEPENDENCIES=("build-essential" "ripgrep" "xclip" "git" "fzf" "libssl-dev" "fuse")
+declare -a NVIM_APT_DEPENDENCIES=("build-essential" "ripgrep" "xclip" "git" "fzf" "libssl-dev")
+# echo "[DEBUG] Neovim dependencies installable with dnf: ${NVIM_APT_DEPENDENCIES[@]}"
+
+## If $INSTALL_NVIM_APPIMG=1, add FUSE dependency
+if [[ "${INSTALL_NVIM_APPIMG} -eq 1" ]]; then
+    echo "Neovim will be installed by AppImage. Install FUSE dependency."
+    if [[ ! " ${NVIM_DNF_DEPENDENCIES[@]} " =~ " fuse " ]]l then
+        ## Add "fuse" to dnf dependencies
+        NVIM_DNF_DEPENDENCIES+=("fuse")
+    fi
+
+    if [[ ! " ${NVIM_APT_DEPENDENCIES[@]} " =~ " fuse " ]]; then
+        echo "Neovim will be installed by AppImage. Install FUSE dependency."
+        ## Add "fuse" to apt dependencies
+        NVIM_APT_DEPENDENCIES+=("fuse")
+    fi
+fi
 
 function return_to_root() {
     cd $CWD
@@ -290,22 +309,77 @@ function install-neovim-appimg() {
     return_to_root
 }
 
+function install-neovim-source() {
+    BUILD_DIR="${CWD}/build"
+
+    if [[ ! -d $BUILD_DIR ]]; then
+        echo "Creating build path: ${BUILD_DIR}"
+        mkdir -pv "${BUILD_DIR}"
+    fi
+
+    declare -a APT_DEPENDS=("git" "ninja-build" "gettext" "libtool" "libtool-bin" "autoconf" "automake" "cmake" "g++" "pkg-config" "unzip" "curl" "doxygen")
+    declare -a DNF_DEPENDS=("git" "ninja-build" "libtool" "autoconf" "automake" "cmake" "gcc" "gcc-c++" "make" "pkgconfig" "unzip" "patch" "gettext" "curl")
+
+    if [[ $PKG_MGR == "dnf" ]]; then
+        echo "Installing build tools for neovim with dnf"
+        sudo dnf install -y "${DNF_DEPENDS[@]}"
+        eval_last $?
+    elif [[ $PKG_MGR == "apt" ]]; then
+        echo "Installing build tools for neovim with apt"
+        sudo apt install -y "${APT_DEPENDS[@]}"
+        eval_last $?
+    else
+        echo "[WARNING] Building neovim from source with package manager '${PKG_MGR}' is not supported."
+        exit 1
+    fi
+
+    cd $BUILD_DIR
+    eval_last $?
+    if [[ ! -d "neovim" ]]; then
+        echo "Cloning neovim repeository"
+        git clone https://github.com/neovim/neovim.git
+    else
+        echo "Neovim repository already exists at ${BUILD_DIR}/neovim. Changes will be pulled, and Neovim will be rebuilt."
+    fi
+
+    cd neovim
+    eval_last $?
+    echo "Running a git pull to get any changes"
+    git pull
+    eval_last $?
+    
+    echo "Building Neovim"
+    make CMAKE_BUILD_TYPE=RelWithDebInfo
+    eval_last $?
+
+    echo "Installing Neovim after building source"
+    sudo make install
+    eval_last $?
+
+    echo "[SUCCESS] Neovim installed" 
+}
+
 function symlink-config() {
     ## Create symbolic link from repository's config/nvim path to ~/.config/nvim
 
     if [[ ! -d "${DOTCONFIG_DIR}" ]]; then
-	echo "Path '${DOTCONFIG_DIR}' does not exist. Creating."
-	mkdir -pv "${DOTCONFIG_DIR}"
-    else	
-	if [[ -d $NVIM_CONFIG_DIR ]]; then
-            echo "Neovim config already exists at $NVIM_CONFIG_DIR"
-        elif [ -L "${NVIM_CONFIG_DIR}" ]; then
-            echo "Neovim path is a symlink. Removing link"
+        echo "Path '${DOTCONFIG_DIR}' does not exist. Creating."
+        mkdir -pv "${DOTCONFIG_DIR}"
+    fi
+
+    if [[ -d $NVIM_CONFIG_DIR ]]; then
+        echo "Neovim config already exists at $NVIM_CONFIG_DIR"
+
+        if [ -L "${NVIM_CONFIG_DIR}" ]; then
+            echo "Neovim config is a symlink. Removing link."
             rm "${NVIM_CONFIG_DIR}"
-        else
-            echo "Neovim path is not a symlink. Backing up to ${NVIM_CONFIG_DIR}.bak"
-            mv "${NVIM_CONFIG_DIR}" "${NVIM_CONFIG_DIR}.bak"
         fi
+    elif [ -L "${NVIM_CONFIG_DIR}" ]; then
+        echo "Neovim path is a symlink. Removing link"
+        rm "${NVIM_CONFIG_DIR}"
+    else
+        echo "Neovim path is not a symlink. Backing up to ${NVIM_CONFIG_DIR}.bak"
+        mv "${NVIM_CONFIG_DIR}" "${NVIM_CONFIG_DIR}.bak"
     fi
 
     echo "Creating symlink from ${NVIM_CONFIG_SRC} to ${NVIM_CONFIG_DIR}"
@@ -394,8 +468,15 @@ function main() {
         exit 1
     fi    
 
-    ## Install neovim from github
-    install-neovim-appimg
+    if [[ $INSTALL_NVIM_APPIMG -eq 1 ]]; then
+        ## Install neovim appimg from github
+        install-neovim-appimg
+    else
+        ## Install neovim from source
+        install-neovim-source
+    fi
+
+    eval_last $?
 
     ## Symlink neovim configuration
     symlink-config
