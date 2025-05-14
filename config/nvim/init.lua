a34f5b7d7131cd9ea1336c162def0058db26edf6
redjax <no@none.com>
2025-05-14 05:06:00 +0000

    Add cosmic UI

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -407,6 +407,45 @@ require('lazy').setup({
   --   end,
   -- },
 
+  -- cosmic UI https://github.com/CosmicNvim/cosmic-ui
+  {
+    'CosmicNvim/cosmic-ui',
+    requires = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('cosmic-ui').setup()
+    end,
+    opts = {
+      -- default border to use
+      -- 'single', 'double', 'rounded', 'solid', 'shadow'
+      border_style = 'single',
+    
+      -- rename popup settings
+      rename = {
+        border = {
+          highlight = 'FloatBorder',
+          style = 'single',
+          title = ' Rename ',
+          title_align = 'left',
+          title_hl = 'FloatBorder',
+        },
+        prompt = '> ',
+        prompt_hl = 'Comment',
+      },
+    
+      code_actions = {
+        min_width = nil,
+        border = {
+          bottom_hl = 'FloatBorder',
+          highlight = 'FloatBorder',
+          style = 'single',
+          title = 'Code Actions',
+          title_align = 'center',
+          title_hl = 'FloatBorder',
+        },
+      }
+    }
+  },
+
   -- Auto mkdir on save https://github.com/mateuszwieloch/automkdir.nvim
   {
     "mateuszwieloch/automkdir.nvim",
