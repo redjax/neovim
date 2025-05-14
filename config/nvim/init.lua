9cd33c7f0661fbba59e8a9289afea5198b060011
redjax <no@none.com>
2025-05-14 01:41:32 +0000

    Update Lua LSP

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1160,9 +1160,10 @@ require('lazy').setup({
         --
 
         lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
+          cmd = { "lua-language-server" },
+          filetypes = { "lua" },
           -- capabilities = {},
+          root_markers = { ".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "stylua.toml", "selene.toml", "selene.yml", ".git" },
           settings = {
             Lua = {
               completion = {
