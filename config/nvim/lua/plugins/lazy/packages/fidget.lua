0f336f1608c6a65b924820789a0203a2b3e5273d
redjax <no@none.com>
2025-05-22 15:16:01 -0400

    Add fidget

---
+++ config/nvim/lua/plugins/lazy/packages/fidget.lua
@@ -0,0 +1,9 @@
+-- Fidget https://github.com/j-hui/fidget.nvim
+
+return {
+    enabled = true,
+    "j-hui/fidget.nvim",
+    -- Load on a general event, or use "LspAttach" for LSP-only
+    event = "VeryLazy",
+    opts = {},
+}
