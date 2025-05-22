f46dc3351a1c1abb5daed9be232879d7d9a94766
redjax <no@none.com>
2025-05-22 18:27:48 -0400

    Manually configure mini.animate speeds

--- config/nvim/lua/plugins/lazy/packages/mini.lua
+++ config/nvim/lua/plugins/lazy/packages/mini.lua
@@ -33,8 +33,33 @@ return {
         require('mini.indentscope').setup()
         -- Startup screen
         -- require('mini.starter').setup()
-        -- Animations
-        require('mini.animate').setup()
+
+        -- Window and cursor animations
+        require('mini.animate').setup({
+            -- All animations have a default duration of 250
+
+            cursor = {
+                enable = true,
+                -- Animation timing: 250ms total duration, linear progression
+                timing = require('mini.animate').gen_timing.linear({ duration = 60, unit = "total" }),
+            },
+            scroll = {
+                enable = true,
+                timing = require('mini.animate').gen_timing.linear({ duration = 35, unit = "total" }),
+            },
+            resize = {
+                enable = true,
+                timing = require('mini.animate').gen_timing.linear({ duration = 60, unit = "total" }),
+            },
+            open = {
+                enable = true,
+                timing = require('mini.animate').gen_timing.linear({ duration = 100, unit = "total" }),
+            },
+            close = {
+                enable = true,
+                timing = require('mini.animate').gen_timing.linear({ duration = 100, unit = "total" }),
+            },
+        })
 
         -- Code minimap
         -- require('mini.map').setup()
