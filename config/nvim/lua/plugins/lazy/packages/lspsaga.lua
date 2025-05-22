c9d7f35db8e2060d5d12272cfc1df74b1e45e5d7
redjax <no@none.com>
2025-05-22 09:29:32 -0400

    Add enabled toggle to lspsaga

--- config/nvim/lua/plugins/lazy/packages/lspsaga.lua
+++ config/nvim/lua/plugins/lazy/packages/lspsaga.lua
@@ -1,6 +1,7 @@
 -- LSPSaga https://github.com/nvimdev/lspsaga.nvim
 
 return {
+    enabled = true,
     "nvimdev/lspsaga.nvim",
     event = "LspAttach", -- loads when LSP attaches to a buffer
     dependencies = {
