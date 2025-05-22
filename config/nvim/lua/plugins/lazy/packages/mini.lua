6781c5b170ab7f48cea58497af076f4b0f367881
redjax <no@none.com>
2025-05-22 11:37:59 -0400

    Add key clues mini module

--- config/nvim/lua/plugins/lazy/packages/mini.lua
+++ config/nvim/lua/plugins/lazy/packages/mini.lua
@@ -51,5 +51,46 @@ return {
         --         MiniMap.open()
         --     end,
         -- })
+
+        -- Next-key clues
+        require('mini.clue').setup({
+            triggers = {
+                -- Leader triggers
+                { mode = 'n', keys = '<Leader>' },
+                { mode = 'x', keys = '<Leader>' },
+                -- Built-in completion
+                { mode = 'i', keys = '<C-x>' },
+                -- `g` key
+                { mode = 'n', keys = 'g' },
+                { mode = 'x', keys = 'g' },
+                -- Marks
+                { mode = 'n', keys = "'" },
+                { mode = 'n', keys = '`' },
+                { mode = 'x', keys = "'" },
+                { mode = 'x', keys = '`' },
+                -- Registers
+                { mode = 'n', keys = '"' },
+                { mode = 'x', keys = '"' },
+                { mode = 'i', keys = '<C-r>' },
+                { mode = 'c', keys = '<C-r>' },
+                -- Window commands
+                { mode = 'n', keys = '<C-w>' },
+                -- `z` key
+                { mode = 'n', keys = 'z' },
+                { mode = 'x', keys = 'z' },
+            },
+            clues = {
+                -- Enhance this by adding descriptions for <Leader> mapping groups
+                require('mini.clue').gen_clues.builtin_completion(),
+                require('mini.clue').gen_clues.g(),
+                require('mini.clue').gen_clues.marks(),
+                require('mini.clue').gen_clues.registers(),
+                require('mini.clue').gen_clues.windows(),
+                require('mini.clue').gen_clues.z(),
+            },
+            window = {
+                delay = 500, -- Show clue window after 500ms (default is 1000ms)
+            },
+        })
     end,
 }
