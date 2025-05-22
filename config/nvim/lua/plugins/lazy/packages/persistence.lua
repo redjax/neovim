1b8884f6f9078e0579185711213d570682fcd078
redjax <no@none.com>
2025-05-22 16:29:00 -0400

    add persistence session management

---
+++ config/nvim/lua/plugins/lazy/packages/persistence.lua
@@ -0,0 +1,24 @@
+-- Persistence https://github.com/folke/persistence.nvim
+
+return {
+    enabled = true,
+    "folke/persistence.nvim",
+    -- Start session management before buffers are read
+    event = "BufReadPre",
+    opts = {
+        -- Directory where session files are saved
+        dir = vim.fn.expand(vim.fn.stdpath("state") .. "/sessions/"),
+        -- Session options
+        options = { "buffers", "curdir", "tabpages", "winsize" },
+        -- Function to run before saving the session
+        pre_save = nil,
+    },
+    config = function(_, opts)
+        require("persistence").setup(opts)
+
+        -- Optional: Keymaps for session management
+        vim.keymap.set("n", "<leader>qs", function() require("persistence").load() end, { desc = "Restore Session" })
+        vim.keymap.set("n", "<leader>ql", function() require("persistence").load({ last = true }) end, { desc = "Restore Last Session" })
+        vim.keymap.set("n", "<leader>qd", function() require("persistence").stop() end, { desc = "Don't Save Current Session" })
+    end,
+}
