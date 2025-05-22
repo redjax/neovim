fe3dac95956e25f4b23fc3dd0465a55e485dd207
redjax <no@none.com>
2025-05-22 16:26:43 -0400

    Add rainbow delimiters

---
+++ config/nvim/lua/plugins/lazy/packages/rainbow-delimiters.lua
@@ -0,0 +1,8 @@
+-- Rainbow Delimiters https://github.com/HiPhish/rainbow-delimiters.nvim
+
+return {
+    enabled = true,
+    "HiPhish/rainbow-delimiters.nvim",
+    -- Or "BufReadPost" if you want it earlier
+    event = "VeryLazy",
+}
