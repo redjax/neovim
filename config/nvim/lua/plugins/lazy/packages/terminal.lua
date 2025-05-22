df5b385844e86ac83c20c1236267d5ab02e0bc3d
redjax <no@none.com>
2025-05-22 09:17:55 -0400

    Disable terminal package

--- config/nvim/lua/plugins/lazy/packages/terminal.lua
+++ config/nvim/lua/plugins/lazy/packages/terminal.lua
@@ -1,7 +1,7 @@
 -- Terminal https://github.com/
 
 return {
-    enabled = true,
+    enabled = false,
     "rebelot/terminal.nvim",
     opts = {
         layout = {
@@ -14,9 +14,12 @@ return {
     },
     config = function(_, opts)
         require("terminal").setup(opts)
-        local term_map = require("terminal.mappings")
-        vim.keymap.set("n", "<leader>tt", term_map.toggle, { noremap = true, silent = true, desc = "Toggle Terminal" })
-        vim.keymap.set("n", "<leader>tr", term_map.run, { noremap = true, silent = true, desc = "Run in Terminal" })
-        -- Add more keymaps as desired
+        -- local term_map = require("terminal.mappings")
+        -- vim.keymap.set("n", "<leader>tt", term_map.toggle, { noremap = true, silent = true, desc = "Toggle Terminal" })
+        -- vim.keymap.set("n", "<leader>tr", term_map.run, { noremap = true, silent = true, desc = "Run in Terminal" })
     end,
+    keys = {
+        { "<leader>tt", function() require("terminal").toggle() end, desc = "Toggle Terminal", noremap = true, silent = true },
+        { "<leader>tr", function() require("terminal").run() end, desc = "Run in Terminal", noremap = true, silent = true },
+    },
 }
