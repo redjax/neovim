eaed2afc975b6f95a22ebd3732652406535830c2
redjax <no@none.com>
2025-05-22 11:47:57 -0400

    Add commented mini.files, re-enable oil

--- config/nvim/lua/plugins/lazy/packages/oil.lua
+++ config/nvim/lua/plugins/lazy/packages/oil.lua
@@ -1,7 +1,7 @@
 -- Oil https://github.com/stevearc/oil.nvim
 
 return {
-    enabled = false,
+    enabled = true,
     "stevearc/oil.nvim",
     lazy = false, -- Recommended: load on startup for best integration
     dependencies = {
