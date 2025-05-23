c03aaeac3139351f460e37cbb4ebdb8a97d04729
redjax <no@none.com>
2025-05-23 14:44:40 -0400

    Update Powershell LSP install script

--- scripts/windows/install-lsp-requirements.ps1
+++ scripts/windows/install-lsp-requirements.ps1
@@ -8,13 +8,58 @@ $NpmDependencies = @(
     "dockerfile-language-server-nodejs",
     "graphql-language-service-cli",
     "pyright",
-    "@stoplight/spectral-cli"
+    "@stoplight/spectral-cli",
+    # "@ansible/ansible-language-server",
+    # "css-language-server",
+    # "cssmodules-language-server",
+    # "eslint",
+    # "@bmewburn/vscode-html-languageserver",
+    # "@aws/lsp-json",
+    # "sql-language-server",
+    # "svelte-language-server"
 )
 
+$PythonDependencies = @(
+    "yamlls",
+    "nginx-language-server",
+    "ruff",
+    "ruff-lsp",
+    "salt-lsp",
+    "sqruff",
+    "cmake-language-server"
+)
+
+if ( Get-Command "uv" ) {
+    Write-Host "Using uv for Python dependencies"
+    $PythonPkgManager = "uv"
+} else {
+    Write-Host "Using pip for Python dependencies"
+    $PythonPkgManager = "pip"
+}
+
+
+## Install node dependencies
 foreach ($NpmPkg in $NpmDependencies ) {
     try {
         npm install -g $NpmPkg
     } catch {
         Write-Error "Error installing NPM dependency '$NpmPkg'. Details: $($Exception.Message)"
+    }
+}
+
+## Install python dependencies
+foreach ($PythonPkg in $PythonDependencies ) {
+    if ($PythonPkgManager -eq "uv") {
+        try {
+            uv tool install $PythonPkg
+        } catch {
+            Write-Error "Error installing Python dependency '$PythonPkg'. Details: $($Exception.Message)"
+        }
+    } else {
+        try {
+            python -m pip install $PythonPkg
+        } catch {
+            Write-Error "Error installing Python dependency '$PythonPkg'. Details: $($Exception.Message)"
+        }
     }
 }
