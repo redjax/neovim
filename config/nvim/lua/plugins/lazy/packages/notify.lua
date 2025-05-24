c05a14acb20d8d69ebf2927aa7a113c651a271f5
redjax <no@none.com>
2025-05-23 20:51:58 -0400

    Update nvim-notify config

--- config/nvim/lua/plugins/lazy/packages/notify.lua
+++ config/nvim/lua/plugins/lazy/packages/notify.lua
@@ -7,9 +7,25 @@ return {
     config = function()
         require("notify").setup({
             -- Customizations (see :h notify.Config)
-            stages = "fade_in_slide_out", -- animation style
-            render = "default",           -- notification style
-            timeout = 3000,               -- time in ms before notification disappears
+
+            -- Max column size
+            max_width = 50,
+            -- Max row size
+            max_height = 10,
+            -- Higher number = faster animation/higher CPU usage. Default = 60
+            fps = 120,
+            -- true = open messages at top of screen false = open messages at bottom
+            top_down = false,
+            -- Log level (debug, error, info, trace, warn, off)
+            -- level = "trace",
+            -- Animation style
+            -- \ Options: fade_in_slide_out, fade, slide, static
+            stages = "fade",
+            -- Notification style
+            -- \ Options: default, minimal, simple, compact, wrapped-compact
+            render = "wrapped-compact",
+            -- Time in ms before notification disappears. Default = 3000
+            timeout = 3000,
             -- background_colour = "#1a1a1a",-- background color (set to "Normal" for default)
         })
         vim.notify = require("notify")
