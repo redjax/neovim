00e6a9efdb0bbfaa1b420122cddd7af5360c964b
redjax <no@none.com>
2025-07-12 16:50:04 +0000

    fix(npm-nvim): Add npm install for neovim to dnf depends function

--- scripts/linux/new.install.sh
+++ scripts/linux/new.install.sh
@@ -377,6 +377,13 @@ function install_dependencies_dnf() {
         echo "[WARNING] tree-sitter is not installed."
         npm install -g tree-sitter-cli
     fi
+
+    ## Install neovim package for npm
+    echo "Installing neovim with npm"
+    npm install -g neovim
+    if [[ $? -ne 0 ]]; then
+        echo "[ERROR] Error installing neovim with npm"
+    fi
 }
 
 function install_neovim_appimg() {
