7fae3365b025798b53dc3f71a9dd346119506859
redjax <5534031+redjax@users.noreply.github.com>
2025-07-25 00:12:23 -0400

    fix(plugins): Add lsp, plugins.

---
+++ config/nvim-work/lua/lsp/lsp-signature.lua
@@ -0,0 +1,23 @@
+-- LSP Signature https://github.com/ray-x/lsp_signature.nvim
+
+return {
+    enabled = true,
+    "ray-x/lsp_signature.nvim",
+    event = "InsertEnter", -- Loads when you enter insert mode
+    opts = {
+        -- Mandatory for borders and floating win config
+        bind = true,
+        handler_opts = {
+            -- "rounded", "single", "double", "shadow", "none", or a border table
+            border = "rounded"
+        },
+        -- Show signature in a floating window
+        floating_window = true,
+        -- Enable virtual text hints
+        hint_enable = true,
+        -- You can change this emoji!
+        hint_prefix = "🐼 ",
+        -- Try to place the window above the current line
+        floating_window_above_cur_line = true,
+    },
+}
---
+++ config/nvim-work/lua/plugins/nvim-web-devicons.lua
@@ -0,0 +1,18 @@
+-- nvim-web-devicons https://github.com/nvim-tree/nvim-web-devicons
+
+return {
+    enabled = true,
+    "nvim-tree/nvim-web-devicons",
+    lazy = false,
+    priority = 900,
+    config = function()
+      require("nvim-web-devicons").setup({
+        -- enable different highlight colors per icon
+        color_icons = true,
+        -- enable default icons for unknown filetypes
+        default = true,
+        -- strict matching by filename/extension
+        strict = true,
+      })
+    end,
+}
