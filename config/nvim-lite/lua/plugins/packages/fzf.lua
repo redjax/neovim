2f18acd93ae68847c9c23d7d72eea1aa57bae923
redjax <5534031+redjax@users.noreply.github.com>
2025-07-24 23:52:08 -0400

    fix(plugins): Add plugin manager & plugins

---
+++ config/nvim-lite/lua/plugins/packages/fzf.lua
@@ -0,0 +1,11 @@
+-- fzf https://github.com/
+
+return {
+    enabled = false,
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    -- or if using mini.icons/mini.nvim
+    -- dependencies = { "echasnovski/mini.icons" },
+    opts = {}
+}
