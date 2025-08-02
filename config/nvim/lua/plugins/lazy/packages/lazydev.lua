2bf9bb807659d528bd9b55bc16e7f4e84a32ac82
redjax <5534031+redjax@users.noreply.github.com>
2025-08-02 01:47:21 -0400

    fix(lazydev): Fix lazydev config

--- config/nvim/lua/plugins/lazy/packages/lazydev.lua
+++ config/nvim/lua/plugins/lazy/packages/lazydev.lua
@@ -1,38 +1,39 @@
--- Lazydev https://github.com/folke/lazydev.nvim
-
 return {
+  -- Lazydev plugin (load only for Lua files)
+  {
     "folke/lazydev.nvim",
-        -- Only load for Lua files (recommended)
-        ft = "lua",
-        opts = {},
-    },
-    -- cmp completions
-    {
-        "hrsh7th/nvim-cmp",
-        opts = function(_, opts)
-        opts.sources = opts.sources or {}
-        table.insert(opts.sources, {
-            name = "lazydev",
-            group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-        })
-        end,
-    },
-    -- Blink completions
-    {
-        "saghen/blink.cmp",
-        opts = {
-            sources = {
-                -- add lazydev to your completion providers
-                default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-                providers = {
-                    lazydev = {
-                        name = "LazyDev",
-                        module = "lazydev.integrations.blink",
-                        -- make lazydev completions top priority (see `:h blink.cmp`)
-                        score_offset = 100,
-                    },
-                },
-            },
+    ft = "lua",
+    opts = {},
+  },
+
+  -- cmp completions
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, {
+        name = "lazydev",
+        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+      })
+    end,
+  },
+
+  -- Blink completions
+  {
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        -- add lazydev to your completion providers
+        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          },
         },
-    }
+      },
+    },
+  },
 }
