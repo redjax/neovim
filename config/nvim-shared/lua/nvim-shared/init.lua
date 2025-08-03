9fc7efa6d8f518fcdbd1ae75d7a253f6898e1c42
redjax <5534031+redjax@users.noreply.github.com>
2025-08-03 00:42:07 -0400

    fix(shared-lsp): Remove lsp import from shared top-level.
    I want to explicitly load the LSP in configs, rather than when I import nvim-shared.

--- config/nvim-shared/lua/nvim-shared/init.lua
+++ config/nvim-shared/lua/nvim-shared/init.lua
@@ -2,11 +2,8 @@
 local platform = require("nvim-shared.config.platform")
 -- Create config object
 local config = require("nvim-shared.config")
--- Create LSP object
-local lsp = require("nvim-shared.lsp")
 
 return {
   platform = platform,
   config = config,
-  lsp = lsp,
 }
