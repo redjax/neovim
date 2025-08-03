e62e53c01740536e51eba2f392038e801e1a3978
redjax <5534031+redjax@users.noreply.github.com>
2025-08-03 16:13:56 -0400

    fix(auto-servers): Add missing LSPs from old config.
    Remove old LSP config.

--- config/nvim-shared/lua/nvim-shared/_old_lsp/lsp_plugins/init.lua
+++
@@ -1,7 +0,0 @@
-return {
-  { import = "nvim-shared.lsp.lsp_plugins.mason" },
-  { import = "nvim-shared.lsp.lsp_plugins.lsp" },
-  { import = "nvim-shared.lsp.lsp_plugins.lsp_signature" },
-  { import = "nvim-shared.lsp.lsp_plugins.none_ls" },
-  { import = "nvim-shared.lsp.lsp_plugins.dap" },
-}
