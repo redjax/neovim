bd6efc151edd2a9a80a4bca7fc1c7eaba3d01021
redjax <5534031+redjax@users.noreply.github.com>
2025-08-04 13:46:32 -0400

    fix(neovide): Move neovide config into options/ dir

--- config/nvim-shared/lua/nvim-shared/neovide/init.lua
+++ config/nvim-shared/lua/nvim-shared/neovide/init.lua
@@ -1,43 +1,9 @@
--- Enable true color support
-vim.o.termguicolors = true
+-- Enable profiler/debug statistics
+vim.g.neovide_profiler = false
 
--- Automatically switch background for light/dark theme (auto, dark, light)
-vim.g.neovide_theme = 'auto'
-
-vim.g.neovide_input_use_logo = true          -- Use "logo" key as a modifier (for custom keybinds)
-vim.g.neovide_profiler = false               -- Enable profiler/debug statistics
-
--- Set window size
-vim.g.neovide_remember_window_size = true
-vim.o.columns  = 80
-vim.o.lines = 24
-vim.g.neovide_scale_factor = 1.0
-
--- Set the font for Neovide (adjust to your preferred font and size)
-vim.o.guifont = "FiraCode Nerd Font:h12"
-
--- Neovide transparency settings
-vim.g.neovide_opacity = 0.8
-
--- Cursor animation settings for Neovide
-vim.g.neovide_cursor_animation_length = 0.15
-vim.g.neovide_cursor_trail_size = 0.8
-vim.g.neovide_cursor_vfx_mode = "wireframe"    -- Other options: "railgun", "torpedo", "particle", "sonicboom"
-vim.g.neovide_cursor_vfx_particle_density = 10
-
--- Enable cursor effects
-vim.g.neovide_cursor_animate_in_insert_mode = true
-
--- Disable Neovide splash screen for faster start
-vim.g.neovide_no_idle = true
-
--- Set Neovide refresh rate
-vim.g.neovide_refresh_rate = 60
-vim.g.neovide_no_idle = false            -- Only redraw when needed (default for performance)
-vim.g.neovide_refresh_rate_idle = 5      -- Refresh rate when application is unfocused
-
--- Custom Neovide window padding
-vim.g.neovide_padding_top = 10
-vim.g.neovide_padding_bottom = 10
-vim.g.neovide_padding_right = 10
-vim.g.neovide_padding_left = 10
+-- Import configs
+require("nvim-shared.neovide.options.window")
+require("nvim-shared.neovide.options.colors")
+require("nvim-shared.neovide.options.font")
+require("nvim-shared.neovide.options.keymap")
+require("nvim-shared.neovide.options.animations")
---
+++ config/nvim-shared/lua/nvim-shared/neovide/options/font.lua
@@ -0,0 +1,2 @@
+-- Set the font for Neovide (adjust to your preferred font and size)
+vim.o.guifont = "FiraCode Nerd Font:h12"
