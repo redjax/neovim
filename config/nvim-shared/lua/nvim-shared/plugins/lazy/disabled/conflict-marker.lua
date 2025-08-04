1ae50ff58e191c051a2774f332e260e2cb2eb053
redjax <5534031+redjax@users.noreply.github.com>
2025-08-04 01:56:43 -0400

    fix(disabled): Add shared disabled plugins

---
+++ config/nvim-shared/lua/nvim-shared/plugins/lazy/disabled/conflict-marker.lua
@@ -0,0 +1,7 @@
+-- Conflict marker https://github.com/rhysd/conflict-marker.vim
+
+return {
+    "rhysd/conflict-marker.vim",
+    -- Loads the plugin when you open a file
+    event = "BufReadPre",
+}
