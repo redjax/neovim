4d6a09ebf91e24623b1ca693241b5b339c48a475
redjax <no@none.com>
2025-08-08 14:32:22 -0400

    fix(theme): Add arctic theme

---
+++ config/nvim/lua/themes/arctic.lua
@@ -0,0 +1,12 @@
+-- Arctic theme https://github.com/rockyzhang24/arctic.nvim
+
+return {
+  "rockyzhang24/arctic.nvim",
+  name = "arctic",
+  dependencies = { "rktjmp/lush.nvim" },
+  name = "arctic",
+  branch = "main",
+  priority = 1000,
+  config = function()
+  end
+}
