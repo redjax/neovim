693f1f747ad00d038913fd781b5b741b918b5131
redjax <5534031+redjax@users.noreply.github.com>
2025-09-14 14:14:27 -0400

    fix(visual-multi): Add visual multicursor plugin

---
+++ config/nvim/lua/plugins/lazy/packages/visual-multi.lua
@@ -0,0 +1,9 @@
+-- Visual multicursor select https://github.com/mg979/vim-visual-multi
+
+return {
+  "mg979/vim-visual-multi",
+  config = function()
+    -- Enable default mappings
+    vim.g.VM_default_mappings = 1
+  end,
+}
