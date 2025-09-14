a1dd2520c4c1afc4ff1ab77332bf347a71e8fe2a
redjax <5534031+redjax@users.noreply.github.com>
2025-09-14 15:16:45 -0400

    fix(hypersonic): Add hypersonic regex explainer

---
+++ config/nvim/lua/plugins/lazy/packages/hypersonic.lua
@@ -0,0 +1,23 @@
+-- Hypersonic regex explainer https://github.com/tomiis4/hypersonic.nvim
+
+return {
+    'tomiis4/Hypersonic.nvim',
+    event = "CmdlineEnter",
+    cmd = "Hypersonic",
+    config = function()
+        require('hypersonic').setup({
+            ---@type 'none'|'single'|'double'|'rounded'|'solid'|'shadow'|table
+            border = 'rounded',
+            ---@type number 0-100
+            winblend = 0,
+            ---@type boolean
+            add_padding = true,
+            ---@type string
+            hl_group = 'Keyword',
+            ---@type string
+            wrapping = '"',
+            ---@type boolean
+            enable_cmdline = true
+        })
+    end
+}
