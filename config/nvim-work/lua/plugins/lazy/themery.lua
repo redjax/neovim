28a46771e43a2d763619764a44e692c0fd0eb47f
redjax <5534031+redjax@users.noreply.github.com>
2025-09-22 11:55:24 -0400

    fix(themery): Set themery to lazily load

--- config/nvim-work/lua/plugins/lazy/themery.lua
+++ config/nvim-work/lua/plugins/lazy/themery.lua
@@ -36,6 +36,9 @@ local theme_menu_items = {
 
 return {
     "zaldih/themery.nvim",
+    lazy = false,  -- Load immediately so it's always available
+    priority = 1000,  -- Load early
+    cmd = "Themery",  -- Also load on command
     config = function()
         require("themery").setup({
             themes = theme_menu_items,
