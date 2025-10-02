4e6f66033d982a4e2e19667d4eb7519261bbe2a4
redjax <no@none.com>
2025-10-02 16:48:25 -0400

    fix(neovide): Add neovide init to lazyvim

--- config/nvim-lazyvim/init.lua
+++ config/nvim-lazyvim/init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Load Neovide-specific config if running in Neovide
+--   https://neovide.dev
+if vim.g.neovide then
+  -- Protected call to require neovide init.lua
+  local ok, _ = pcall(require, "neovide.init")
+  if not ok then
+    vim.notify("Failed to load Neovide config", vim.log.levels.WARN)
+  end
+end
