9c15bf21c1098d138f587538efd7299d15712007
redjax <5534031+redjax@users.noreply.github.com>
2026-02-10 09:49:02 -0500

    fix(pairs): Update nvim-work profile's autopairs

--- config/nvim-work/lua/plugins/lazy/autoclose.lua
+++ config/nvim-work/lua/plugins/lazy/autoclose.lua
@@ -2,7 +2,31 @@
 
 return {
   "m4xshen/autoclose.nvim",
+  event = "InsertEnter",
   config = function()
-    require("autoclose").setup()
+    require("autoclose").setup({
+      keys = {
+        ["("] = { escape = false, close = true, pair = "()" },
+        ["["] = { escape = false, close = true, pair = "[]" },
+        ["{"] = { escape = false, close = true, pair = "{}" },
+
+        [">"] = { escape = true, close = false, pair = "<>" },
+        [")"] = { escape = true, close = false, pair = "()" },
+        ["]"] = { escape = true, close = false, pair = "[]" },
+        ["}"] = { escape = true, close = false, pair = "{}" },
+
+        ['"'] = { escape = true, close = true, pair = '""' },
+        ["'"] = { escape = true, close = false, pair = "''" },  -- Disable auto-close for single quotes
+        ["`"] = { escape = true, close = true, pair = "``" },
+      },
+      options = {
+        disabled_filetypes = { "text", "markdown" },
+        disable_when_touch = true,
+        touch_regex = "[%w]",
+        pair_spaces = false,
+        auto_indent = true,
+        disable_command_mode = false,
+      },
+    })
   end,
 }
