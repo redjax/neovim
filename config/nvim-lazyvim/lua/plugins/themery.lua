81b1da6b9edf0c50000a0759291ad899e5f2762a
redjax <5534031+redjax@users.noreply.github.com>
2026-02-12 13:55:32 -0500

    fix(theme): Fix theme in nvim-lazyvim profile

--- config/nvim-lazyvim/lua/plugins/themery.lua
+++ config/nvim-lazyvim/lua/plugins/themery.lua
@@ -3,27 +3,27 @@
 -- You can re-add more themes if needed, but this improves startup time significantly
 local theme_menu_items = {
   -- Aura variants
-  "aura-dark",
-  "aura-soft-dark",
+  { name = "Aura Dark", colorscheme = "aura-dark" },
+  { name = "Aura Soft Dark", colorscheme = "aura-soft-dark" },
   
   -- Catppuccin variants
-  "catppuccin-mocha",
-  "catppuccin-macchiato",
+  { name = "Catppuccin Mocha", colorscheme = "catppuccin-mocha" },
+  { name = "Catppuccin Macchiato", colorscheme = "catppuccin-macchiato" },
   
   -- Popular themes
-  "dracula",
-  "eldritch",
-  "gruvbox",
-  "kanagawa-wave",
-  "nightfly",
-  "nord",
-  "onedark",
-  "one_monokai",
-  "oxocarbon",
-  "tokyonight",
-  "tokyonight-storm", 
-  "tokyonight-moon",
-  "vscode_modern",
+  { name = "Dracula", colorscheme = "dracula" },
+  { name = "Eldritch", colorscheme = "eldritch" },
+  { name = "Gruvbox", colorscheme = "gruvbox" },
+  { name = "Kanagawa Wave", colorscheme = "kanagawa-wave" },
+  { name = "Nightfly", colorscheme = "nightfly" },
+  { name = "Nord", colorscheme = "nord" },
+  { name = "OneDark", colorscheme = "onedark" },
+  { name = "One Monokai", colorscheme = "one_monokai" },
+  { name = "Oxocarbon", colorscheme = "oxocarbon" },
+  { name = "Tokyo Night", colorscheme = "tokyonight" },
+  { name = "Tokyo Night Storm", colorscheme = "tokyonight-storm" },
+  { name = "Tokyo Night Moon", colorscheme = "tokyonight-moon" },
+  { name = "VSCode Modern", colorscheme = "vscode_modern" },
 }
 
 return {
@@ -31,7 +31,7 @@ return {
   {
     "zaldih/themery.nvim",
     lazy = false,  -- Load at startup to restore saved theme
-    priority = 1002,  -- Load after themes but before other plugins
+    priority = 999,  -- Load AFTER themes (which have priority 1000)
     cmd = "Themery",
     config = function()
       require("themery").setup({
