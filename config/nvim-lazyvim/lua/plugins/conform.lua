9594eb11c12d5fb8549361abeb562c460f51d337
redjax <5534031+redjax@users.noreply.github.com>
2026-03-13 15:16:16 -0400

    fix(go): Fix conform issues with go lsp (and a few others)

--- config/nvim-lazyvim/lua/plugins/conform.lua
+++ config/nvim-lazyvim/lua/plugins/conform.lua
@@ -15,76 +15,92 @@ return {
       desc = "Format buffer",
     },
   },
-  opts = {
-    -- Map of filetype to formatters
-    formatters_by_ft = {
-      -- Lua
-      lua = { "stylua" },
-      
-      -- Python
-      python = { "isort", "black" },
-      
-      -- JavaScript/TypeScript
-      javascript = { "prettier" },
-      typescript = { "prettier" },
-      javascriptreact = { "prettier" },
-      typescriptreact = { "prettier" },
-      
-      -- Web technologies
-      html = { "prettier" },
-      css = { "prettier" },
-      scss = { "prettier" },
-      json = { "prettier" },
-      jsonc = { "prettier" },
-      yaml = { "prettier" },
-      yml = { "prettier" },
-      markdown = { "prettier" },
-      
-      -- Rust
-      rust = { "rustfmt" },
-      
-      -- Shell
-      sh = { "shfmt" },
-      bash = { "shfmt" },
-      zsh = { "shfmt" },
-      
-      -- PowerShell
-      ps1 = { "powershell_es" },
-      psm1 = { "powershell_es" },
-      
-      -- Terraform
-      terraform = { "terraform_fmt" },
-      tf = { "terraform_fmt" },
-      
-      -- SQL
-      sql = { "sqlfmt" },
-      
-      -- Docker
-      dockerfile = { "dockerls" },
-      
-      -- C/C++
-      c = { "clang_format" },
-      cpp = { "clang_format" },
-      
-      -- XML
-      xml = { "xmllint" },
-    },
-    
-    -- Custom formatters
-    formatters = {
-      powershell_es = {
+  opts = function(_, opts)
+    local function has(cmd)
+      return vim.fn.executable(cmd) == 1
+    end
+
+    opts.formatters_by_ft = opts.formatters_by_ft or {}
+
+    -- Lua
+    opts.formatters_by_ft.lua = { "stylua" }
+
+    -- Python
+    if has("isort") or has("black") then
+      opts.formatters_by_ft.python = { "isort", "black" }
+    end
+
+    -- JavaScript/TypeScript (only if prettier is available)
+    if has("prettier") or has("prettierd") then
+      local prettier = has("prettierd") and "prettierd" or "prettier"
+      opts.formatters_by_ft.javascript = { prettier }
+      opts.formatters_by_ft.typescript = { prettier }
+      opts.formatters_by_ft.javascriptreact = { prettier }
+      opts.formatters_by_ft.typescriptreact = { prettier }
+      opts.formatters_by_ft.html = { prettier }
+      opts.formatters_by_ft.css = { prettier }
+      opts.formatters_by_ft.scss = { prettier }
+      opts.formatters_by_ft.json = { prettier }
+      opts.formatters_by_ft.jsonc = { prettier }
+      opts.formatters_by_ft.yaml = { prettier }
+      opts.formatters_by_ft.yml = { prettier }
+      opts.formatters_by_ft.markdown = { prettier }
+    end
+
+    -- Rust
+    if has("rustfmt") then
+      opts.formatters_by_ft.rust = { "rustfmt" }
+    end
+
+    -- Shell
+    if has("shfmt") then
+      opts.formatters_by_ft.sh = { "shfmt" }
+      opts.formatters_by_ft.bash = { "shfmt" }
+      opts.formatters_by_ft.zsh = { "shfmt" }
+    end
+
+    -- PowerShell
+    if has("pwsh") then
+      opts.formatters_by_ft.ps1 = { "powershell_es" }
+      opts.formatters_by_ft.psm1 = { "powershell_es" }
+      opts.formatters = opts.formatters or {}
+      opts.formatters.powershell_es = {
         command = "pwsh",
         args = { "-Command", "Format-Document" },
         stdin = true,
-      },
-    },
-    
+      }
+    end
+
+    -- Terraform
+    if has("terraform") then
+      opts.formatters_by_ft.terraform = { "terraform_fmt" }
+      opts.formatters_by_ft.tf = { "terraform_fmt" }
+    end
+
+    -- SQL
+    if has("sqlfmt") then
+      opts.formatters_by_ft.sql = { "sqlfmt" }
+    end
+
+    -- C/C++
+    if has("clang-format") then
+      opts.formatters_by_ft.c = { "clang_format" }
+      opts.formatters_by_ft.cpp = { "clang_format" }
+    end
+
+    -- XML
+    if has("xmllint") then
+      opts.formatters_by_ft.xml = { "xmllint" }
+    end
+
     -- Log level for debugging
-    log_level = vim.log.levels.WARN,
-    
+    opts.log_level = vim.log.levels.WARN
+
     -- Notify on error
-    notify_on_error = true,
-  },
+    opts.notify_on_error = true
+
+    return opts
+  end,
   init = function()
     -- If you want the formatexpr, here is the place to set it
     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
