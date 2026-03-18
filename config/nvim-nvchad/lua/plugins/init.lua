b59d0f3bd9fce59eca336397e166796e3fae7db0
redjax <5534031+redjax@users.noreply.github.com>
2026-03-18 14:22:13 -0400

    nvchad: Add mason LSP tooling and linting configs

--- config/nvim-nvchad/lua/plugins/init.lua
+++ config/nvim-nvchad/lua/plugins/init.lua
@@ -6,8 +6,68 @@ return {
 
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+    },
     config = function()
       require "configs.lspconfig"
     end,
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "html",
+        "cssls",
+        "pyright",
+        "gopls",
+        "rust_analyzer",
+        "bashls",
+        "yamlls",
+        "jsonls",
+        "dockerls",
+        "docker_compose_language_service",
+        "terraformls",
+        "powershell_es",
+        "bicep",
+        "sqlls",
+        "eslint",
+      },
+      automatic_installation = true,
+    },
+  },
+
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require "configs.lint"
+    end,
+  },
+
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      ensure_installed = {
+        -- Formatters
+        "stylua",
+        "black",
+        "isort",
+        "prettier",
+        "shfmt",
+        -- Linters
+        "shellcheck",
+        "flake8",
+        "hadolint",
+        "yamllint",
+        "markdownlint",
+        "actionlint",
+        "jsonlint",
+      },
+    },
   },
 }
