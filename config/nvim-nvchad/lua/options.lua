b6409267f1786224593fda316e9fd3c195accb86
redjax <5534031+redjax@users.noreply.github.com>
2026-03-18 14:55:59 -0400

    nvchad: Add filetype detection & filetype defaults

--- config/nvim-nvchad/lua/options.lua
+++ config/nvim-nvchad/lua/options.lua
@@ -2,3 +2,10 @@ require "nvchad.options"
 
 local o = vim.o
 o.cursorlineopt = "both"
+
+-- Default indent (2 spaces), overridden per-filetype in after/ftplugin/
+o.tabstop = 2
+o.shiftwidth = 2
+o.softtabstop = 2
+o.expandtab = true
+o.smartindent = true
