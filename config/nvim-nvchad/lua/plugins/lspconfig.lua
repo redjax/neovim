26d1f8d1dce6cafe2ec0c9cee5cf889571c67aca
redjax <5534031+redjax@users.noreply.github.com>
2026-03-19 11:43:59 -0400

    schemastore: Add schemastore plugin to nvchad

--- config/nvim-nvchad/lua/plugins/lspconfig.lua
+++ config/nvim-nvchad/lua/plugins/lspconfig.lua
@@ -2,6 +2,7 @@ return {
   "neovim/nvim-lspconfig",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
+    "b0o/schemastore.nvim",
   },
   config = function()
     require "configs.lspconfig"
