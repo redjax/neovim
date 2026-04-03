d9fcf2582b75c3a15061b83c3fbb1cbb06dd7905
redjax <5534031+redjax@users.noreply.github.com>
2026-04-03 16:09:56 -0400

    fix(nvchad-tree): Add Space+e toggle tree keybind

--- config/nvim-nvchad/lua/plugins/init.lua
+++ config/nvim-nvchad/lua/plugins/init.lua
@@ -8,6 +8,21 @@ return {
       opts.actions = opts.actions or {}
       opts.actions.open_file = opts.actions.open_file or {}
       opts.actions.open_file.quit_on_open = true
+
+      -- Add the keymap from the lazy side
+      opts.on_attach = function(bufnr)
+        local api = require("nvim-tree.api")
+        local function opts_desc(desc)
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr }
+        end
+
+        api.config.mappings.default_on_attach(bufnr)
+
+        -- Map Space+e to toggle from inside nvim-tree
+        local map = vim.keymap.set
+        map("n", "<leader>e", api.tree.toggle, opts_desc("Toggle NvimTree"))
+      end
+
       return opts
     end,
   }
