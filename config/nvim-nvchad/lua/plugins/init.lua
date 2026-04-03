523746095107369c5bca71eb40ce76eff2c1fb22
redjax <5534031+redjax@users.noreply.github.com>
2026-04-03 14:59:58 -0400

    fix(nvchad-tree): Update nvim-tree config
    Close when a file is opened

--- config/nvim-nvchad/lua/plugins/init.lua
+++ config/nvim-nvchad/lua/plugins/init.lua
@@ -1,3 +1,14 @@
 -- NvChad loads all *.lua plugins in the lua/plugins/ directory. Each plugin has its own file
 -- to make it easier to manage. Any plugins in the lua/plugins/disabled/ directory will be skipped.
-return {}
+return {
+  {
+  "nvim-tree/nvim-tree.lua",
+  opts = function()
+      local opts = require "nvchad.configs.nvimtree"
+      opts.actions = opts.actions or {}
+      opts.actions.open_file = opts.actions.open_file or {}
+      opts.actions.open_file.quit_on_open = true
+      return opts
+    end,
+  }
+}
