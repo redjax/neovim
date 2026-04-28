034b778f5e1d53773f7a06090db0828e15dc56bb
redjax <5534031+redjax@users.noreply.github.com>
2026-04-28 14:44:28 -0400

    feat(plugin): Add genghis plugin

---
+++ config/nvim12/lua/plugins/genghis.lua
@@ -0,0 +1,42 @@
+-- Genghis [https://github.com/chrisgrieser/nvim-genghis](https://github.com/chrisgrieser/nvim-genghis)
+
+return {
+  src = "https://github.com/chrisgrieser/nvim-genghis",
+  name = "nvim-genghis",
+
+  setup = function()
+    require("genghis").setup({
+      trashCmd = function()
+        if jit.os == "OSX" then
+          return "trash"
+        end
+        if jit.os == "Windows" then
+          return "trash"
+        end
+        if jit.os == "Linux" then
+          return { "gio", "trash" }
+        end
+        return "trash-cli"
+      end,
+      navigation = {
+        onlySameExtAsCurrentFile = false,
+        ignoreDotfiles = true,
+        ignoreExt = { "png", "svg", "webp", "jpg", "jpeg", "gif", "pdf", "zip", ".DS_Store" },
+      },
+      successNotifications = true,
+      icons = {
+        chmodx = "󰒃",
+        copyFile = "󱉥",
+        copyPath = "󰅍",
+        duplicate = "",
+        file = "󰈔",
+        move = "󰪹",
+        new = "󰝒",
+        nextFile = "󰖽",
+        prevFile = "󰖿",
+        rename = "󰑕",
+        trash = "󰩹",
+      },
+    })
+  end,
+}
