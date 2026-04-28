7bd885a83a9c96423502a90bae7e445f7cb81c32
redjax <5534031+redjax@users.noreply.github.com>
2026-04-28 14:05:40 -0400

    chore(format): Format diffview lua file

--- config/nvim12/lua/plugins/diffview.lua
+++ config/nvim12/lua/plugins/diffview.lua
@@ -1,5 +1,4 @@
 -- Diffview https://github.com/sindrets/diffview.nvim
-
 return {
   src = "https://github.com/sindrets/diffview.nvim",
   name = "diffview.nvim",
@@ -10,36 +9,44 @@ return {
       view = {
         default = {
           layout = "diff2_horizontal",
-          winbar_info = false,
+          winbar_info = false
         },
-          merge_tool = {
+        merge_tool = {
           layout = "diff3_horizontal",
-          winbar_info = true,
+          winbar_info = true
         },
-          file_history = {
+        file_history = {
           layout = "diff2_horizontal",
-          winbar_info = false,
-        },
+          winbar_info = false
+        }
       },
       file_panel = {
         listing_style = "tree",
         win_config = {
           position = "left",
-          width = 35,
-        },
+          width = 35
+        }
       },
       file_history_panel = {
         win_config = {
           position = "bottom",
-          height = 16,
-        },
-      },
+          height = 16
+        }
+      }
     })
 
     -- Recommended keymaps for quick access
-    vim.keymap.set("n", "<leader>gd", "<cmd>DiffviewOpen<cr>", { desc = "Open Diffview" })
-    vim.keymap.set("n", "<leader>gD", "<cmd>DiffviewFileHistory %<cr>", { desc = "File History (current file)" })
-    vim.keymap.set("n", "<leader>gH", "<cmd>DiffviewFileHistory<cr>", { desc = "File History (project)" })
-    vim.keymap.set("n", "<leader>gq", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" })
-  end,
+    vim.keymap.set("n", "<leader>gd", "<cmd>DiffviewOpen<cr>", {
+      desc = "Open Diffview"
+    })
+    vim.keymap.set("n", "<leader>gD", "<cmd>DiffviewFileHistory %<cr>", {
+      desc = "File History (current file)"
+    })
+    vim.keymap.set("n", "<leader>gH", "<cmd>DiffviewFileHistory<cr>", {
+      desc = "File History (project)"
+    })
+    vim.keymap.set("n", "<leader>gq", "<cmd>DiffviewClose<cr>", {
+      desc = "Close Diffview"
+    })
+  end
 }
