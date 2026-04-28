76e53fb459d90487a935850c38555b08ab5f4ab1
redjax <5534031+redjax@users.noreply.github.com>
2026-04-28 14:50:55 -0400

    feat(plugin): Add indent-blankline plugin

---
+++ config/nvim12/lua/plugins/indent-blankline.lua
@@ -0,0 +1,10 @@
+-- Indent blankline https://github.com/lukas-reineke/indent-blankline.nvim
+
+return {
+  src = "https://github.com/lukas-reineke/indent-blankline.nvim",
+  name = "indent-blankline.nvim",
+
+  setup = function()
+    require("ibl").setup({})
+  end,
+}
