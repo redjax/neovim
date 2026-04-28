4c77e999594bbb22e6731f893405fc3d1ad04bd8
redjax <5534031+redjax@users.noreply.github.com>
2026-04-28 13:44:58 -0400

    feat(plugin): Add conform plugin

---
+++ config/nvim12/lua/plugins/conform.lua
@@ -0,0 +1,76 @@
+-- Conform.nvim - Lightweight yet powerful formatter plugin
+-- https://github.com/stevearc/conform.nvim
+
+return {
+  src = "https://github.com/stevearc/conform.nvim",
+  name = "conform.nvim",
+  setup = function()
+    -- Ensure plugin is loaded for commands and functionality
+    vim.cmd("packadd conform.nvim")
+
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        html = { "prettier" },
+        css = { "prettier" },
+        scss = { "prettier" },
+        json = { "prettier" },
+        jsonc = { "prettier" },
+        yaml = { "prettier" },
+        yml = { "prettier" },
+        markdown = { "prettier" },
+        go = { "gofumpt", "goimports" },
+        rust = { "rustfmt" },
+        sh = { "shfmt" },
+        bash = { "shfmt" },
+        zsh = { "shfmt" },
+        ps1 = { "powershell_es" },
+        psm1 = { "powershell_es" },
+        terraform = { "terraform_fmt" },
+        tf = { "terraform_fmt" },
+        sql = { "sqlfmt" },
+        c = { "clang_format" },
+        cpp = { "clang_format" },
+        xml = { "xmllint" },
+      },
+      formatters = {
+        powershell_es = {
+          command = "pwsh",
+          args = { "-Command", "Format-Document" },
+          stdin = true,
+        },
+      },
+      format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
+        local disable_filetypes = { "sql", "terraform" }
+        if vim.tbl_contains(disable_filetypes, vim.bo[bufnr].filetype) then return end
+        return { timeout_ms = 500, lsp_format = "fallback" }
+      end,
+      log_level = vim.log.levels.WARN,
+      notify_on_error = true,
+    })
+
+    -- Handle Formatexpr
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+    -- User commands for toggling
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then vim.b.disable_autoformat = true else vim.g.disable_autoformat = true end
+    end, { desc = "Disable autoformat-on-save", bang = true })
+
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, { desc = "Re-enable autoformat-on-save" })
+
+    -- Manual Keybinding registration
+    vim.keymap.set({ "n", "v" }, "<leader>f", function()
+      require("conform").format({ async = true, lsp_format = "fallback" })
+    end, { desc = "Format buffer" })
+  end,
+}
