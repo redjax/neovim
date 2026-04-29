8f32a9844119f86c58bae1c2b7c3439cbf20f896
redjax <5534031+redjax@users.noreply.github.com>
2026-04-29 00:12:32 -0400

    feat(buff-cycle): Add keybind for tab/shift-tab to cycle buffers

--- config/nvim-noplugins/lua/config/keymap.lua
+++ config/nvim-noplugins/lua/config/keymap.lua
@@ -2,23 +2,54 @@
 vim.g.mapleader = " " -- Space as leader
 
 -- Save file with <leader>w
-vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', { desc = 'Save file' })
+vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', {
+  desc = 'Save file'
+})
 
 -- Copy to system clipboard
-vim.keymap.set({'n', 'x'}, 'gy', '"+y', { desc = 'Copy to clipboard' })
+vim.keymap.set({'n', 'x'}, 'gy', '"+y', {
+  desc = 'Copy to clipboard'
+})
 
 -- Paste from system clipboard
-vim.keymap.set({'n', 'x'}, 'gp', '"+p', { desc = 'Paste from clipboard' })
+vim.keymap.set({'n', 'x'}, 'gp', '"+p', {
+  desc = 'Paste from clipboard'
+})
 
 -- Delete without affecting registers
-vim.keymap.set({'n', 'x'}, 'x', '"_x', { desc = 'Delete without yank' })
-vim.keymap.set({'n', 'x'}, 'X', '"_d', { desc = 'Delete line without yank' })
+vim.keymap.set({'n', 'x'}, 'x', '"_x', {
+  desc = 'Delete without yank'
+})
+vim.keymap.set({'n', 'x'}, 'X', '"_d', {
+  desc = 'Delete line without yank'
+})
 
 -- Select all
-vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>', { desc = 'Select all' })
+vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>', {
+  desc = 'Select all'
+})
 
 -- Home & end keys
-vim.keymap.set('n', '<Home>', '^', { desc = 'Go to first non-blank character' })
-vim.keymap.set('n', '<End>', '$', { desc = 'Go to end of line' })
-vim.keymap.set('i', '<Home>', '<C-o>^', { desc = 'Insert: go to start of line' })
-vim.keymap.set('i', '<End>', '<C-o>$', { desc = 'Insert: go to end of line' })
+vim.keymap.set('n', '<Home>', '^', {
+  desc = 'Go to first non-blank character'
+})
+vim.keymap.set('n', '<End>', '$', {
+  desc = 'Go to end of line'
+})
+vim.keymap.set('i', '<Home>', '<C-o>^', {
+  desc = 'Insert: go to start of line'
+})
+vim.keymap.set('i', '<End>', '<C-o>$', {
+  desc = 'Insert: go to end of line'
+})
+
+-- Use Tab and Shift+Tab to cycle between buffers
+vim.keymap.set("n", "<Tab>", "<cmd>bnext<cr>", {
+  silent = true,
+  desc = "Next buffer"
+})
+vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<cr>", {
+  silent = true,
+  desc = "Previous buffer"
+})
+
